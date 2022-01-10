353eb486951da2c335be6bb81677777229d0f4bf
gggao <gggao@telenav.cn>
2022-01-10 17:56:47 +0800

    add video activity

--- app/src/main/java/com/ggg/video/activity/VideoActivity.kt
+++ app/src/main/java/com/ggg/video/activity/VideoActivity.kt
@@ -1,8 +1,6 @@
 package com.ggg.video.activity
 
 import android.os.Bundle
-import android.view.SurfaceView
-import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ScrollState
@@ -10,10 +8,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.unit.dp
 import com.ggg.mediafactory.ui.theme.MediaFactoryTheme
+import com.ggg.video.player.VideoPlayer
 
 class VideoActivity : ComponentActivity() {
+    private val player = VideoPlayer(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -25,19 +25,7 @@ class VideoActivity : ComponentActivity() {
     private fun Content() {
         MediaFactoryTheme(title = "${intent.getStringExtra("title")}") {
             Column(modifier = Modifier.verticalScroll(ScrollState(0))) {
-                AndroidView(factory = {
-                    return@AndroidView LinearLayout(it).apply {
-                        layoutParams.width = LinearLayout.LayoutParams.MATCH_PARENT
-                        layoutParams.height = 100
-                        addView(SurfaceView(it).apply {
-                            layoutParams.width = LinearLayout.LayoutParams.MATCH_PARENT
-                            layoutParams.height = LinearLayout.LayoutParams.MATCH_PARENT
-
-                        })
-                    }
-
-
-                })
+                player.InitPlayer(300.dp, "")
             }
         }
     }
---
+++ app/src/main/java/com/ggg/video/player/VideoPlayer.kt
@@ -0,0 +1,93 @@
+package com.ggg.video.player
+
+import android.content.Context
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.widget.LinearLayout
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.viewinterop.AndroidView
+
+
+/**
+ * Created by  gggao on 1/10/2022.
+ */
+class VideoPlayer(private val context: Context) {
+    private var surfaceHolder: SurfaceHolder? = null
+    private val mediaPlayer = MediaPlayer().apply {
+        val attributes = AudioAttributes.Builder()
+            .setLegacyStreamType(AudioManager.STREAM_MUSIC)
+            .build()
+        setAudioAttributes(attributes)
+    }
+
+    fun resetVideoPath(path: String) {
+        mediaPlayer.reset()
+        mediaPlayer.setDataSource(path)
+        mediaPlayer.prepare()
+        mediaPlayer.start()
+    }
+
+    @Composable
+    fun InitPlayer(height: Dp, videoPath: String) {
+        AndroidView(
+            factory = {
+                return@AndroidView LinearLayout(it).apply {
+                    layoutParams =
+                        LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.MATCH_PARENT
+                        )
+
+                    addView(SurfaceView(it).apply {
+                        layoutParams =
+                            LinearLayout.LayoutParams(
+                                LinearLayout.LayoutParams.MATCH_PARENT,
+                                LinearLayout.LayoutParams.MATCH_PARENT
+                            )
+                        surfaceHolder = holder
+                        holder.addCallback(object : SurfaceHolder.Callback {
+                            override fun surfaceCreated(holder: SurfaceHolder) {
+                                if (videoPath.isNotEmpty()) {
+                                    play(videoPath)
+                                }
+                            }
+
+                            override fun surfaceChanged(
+                                holder: SurfaceHolder,
+                                format: Int,
+                                width: Int,
+                                height: Int
+                            ) {
+
+                            }
+
+                            override fun surfaceDestroyed(holder: SurfaceHolder) {
+                                mediaPlayer.stop()
+                                mediaPlayer.release()
+                            }
+
+                        })
+                    })
+                }
+
+
+            }, modifier = Modifier
+                .fillMaxWidth()
+                .height(height)
+        )
+    }
+
+    private fun play(videoPath: String) {
+        mediaPlayer.setDisplay(surfaceHolder)
+        mediaPlayer.setDataSource(videoPath)
+        mediaPlayer.prepare()
+        mediaPlayer.start()
+    }
+}
