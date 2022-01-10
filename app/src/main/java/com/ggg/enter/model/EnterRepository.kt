581673b64541bb6cbe4dd11a3077350be803d2d5
gggao <gggao@telenav.cn>
2022-01-10 17:56:47 +0800

    add video activity

--- app/src/main/java/com/ggg/enter/model/EnterRepository.kt
+++ app/src/main/java/com/ggg/enter/model/EnterRepository.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import com.ggg.MainApplication
 import com.ggg.image.activity.ImageActivity
 import com.ggg.mediafactory.R
+import com.ggg.video.activity.VideoActivity
 
 
 /**
@@ -13,10 +14,17 @@ class EnterRepository {
     fun obtainEnters(): List<EnterBean> {
         val activity = MainApplication.getCurrentActivity()
         return listOf(
-            EnterBean(activity?.getString(R.string.original_to_film) ?: "") {
+            EnterBean(activity?.getString(R.string.image_handler) ?: "") {
                 MainApplication.getCurrentActivity()?.apply {
                     startActivity(Intent(this, ImageActivity::class.java).apply {
-                        putExtra("title", getString(R.string.original_to_film))
+                        putExtra("title", getString(R.string.image_handler))
+                    })
+                }
+            },
+            EnterBean(activity?.getString(R.string.video_handler) ?: "") {
+                MainApplication.getCurrentActivity()?.apply {
+                    startActivity(Intent(this, VideoActivity::class.java).apply {
+                        putExtra("title", getString(R.string.video_handler))
                     })
                 }
             }
