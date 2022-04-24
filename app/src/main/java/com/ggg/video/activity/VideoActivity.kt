21353d73901b04a6efe2746d8febf69ee6684f17
gggao <gggao@telenav.cn>
2022-04-24 20:24:03 +0800

    add thread

--- app/src/main/java/com/ggg/video/activity/VideoActivity.kt
+++ app/src/main/java/com/ggg/video/activity/VideoActivity.kt
@@ -2,28 +2,30 @@ package com.ggg.video.activity
 
 import android.os.Bundle
 import android.os.Environment
+import android.view.Gravity
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.ggg.handler.MediaHandler
 import com.ggg.mediafactory.ui.theme.MediaFactoryTheme
 import com.ggg.video.player.VideoPlayer
 import com.ggg.video.viewmodel.VideoSourceViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import java.io.File
+import kotlinx.coroutines.launch
 
 class VideoActivity : ComponentActivity() {
     private val viewModel = VideoSourceViewModel()
@@ -41,31 +43,71 @@ class VideoActivity : ComponentActivity() {
 
     @Composable
     private fun Content() {
-        MediaFactoryTheme(title = "${intent.getStringExtra("title")}") {
-            val video: Flow<String> by remember {
-                mutableStateOf(viewModel.getVideoPath())
-            }
+        var showLoading by remember {
+            mutableStateOf(false)
+        }
+        val video: Flow<String> by remember {
+            mutableStateOf(viewModel.getVideoPath())
+        }
 
+        MediaFactoryTheme(title = "${intent.getStringExtra("title")}") {
             Column(modifier = Modifier.verticalScroll(ScrollState(0))) {
                 player.InitPlayer(300.dp, video)
                 Spacer(modifier = Modifier.height(20.dp))
                 Button(onClick = {
-
-                    if (MediaHandler.sendVideoCommand(
+                    showLoading = true
+                    val coroutineScope = CoroutineScope(Dispatchers.IO)
+                    coroutineScope.launch {
+                        MediaHandler.sendVideoCommand(
                             MediaHandler.DecodeCommand(
                                 player.currentVideoPath,
                                 outPath
-                            )
-                        ) == 0
-                    ) {
-                        Toast.makeText(this@VideoActivity, "decode success", Toast.LENGTH_LONG)
-                            .show()
+                            ), object : MediaHandler.OnHandlerResultListener {
+                                override fun onResult(code: Int) {
+                                    val text = if (code == 0) {
+                                        "decode success"
+                                    } else {
+                                        "decode failed"
+                                    }
+
+                                    coroutineScope.launch(Dispatchers.Main) {
+                                        showLoading = false
+                                        Toast.makeText(
+                                            this@VideoActivity, text, Toast.LENGTH_LONG
+                                        ).apply {
+                                            setGravity(Gravity.CENTER, 0, 0)
+                                        }.show()
+                                    }
+
+                                }
+
+                            }
+                        )
                     }
+
+
                 }) {
                     Text(text = "decoder")
                 }
+
+                if (showLoading) {
+                    dialog()
+                }
             }
         }
+
     }
 
+    @Composable
+    private fun dialog() {
+        Dialog(
+            onDismissRequest = {},
+            DialogProperties(dismissOnBackPress = false, dismissOnClickOutside = false)
+        ) {
+            Box(modifier = Modifier.size(300.dp), contentAlignment = Alignment.Center) {
+                CircularProgressIndicator()
+            }
+        }
+
+    }
 }
--- handler/src/main/cpp/video/video_handler.cpp
+++ handler/src/main/cpp/video/video_handler.cpp
@@ -4,47 +4,108 @@
 
 #include "video_handler.h"
 #include "video_dealer.h"
+#include <thread>
 
+jobject gl_command;
+jclass gl_command_clazz;
+jclass gl_resultListener_clazz;
+jobject gl_result_listener;
+JavaVM *current_jvm;
 
-JNIEXPORT jint JNICALL videoCommandHandler(JNIEnv *env, jobject clazz, jobject command) {
+bool getEnv(JNIEnv **env) {
+    JavaVMAttachArgs args;
+    args.version = JNI_VERSION_1_6;
+    args.name = "thread-handler_video";
+    args.group = nullptr;
+    int status = current_jvm->GetEnv((void **) env, JNI_VERSION_1_6);
+    LOGD("thread:", "GetEnv status:%d\n", status);
+    if (status != JNI_OK) {
+        status = current_jvm->AttachCurrentThread(env, &args);
+        LOGD("thread:", "AttachCurrentThread status:%d\n", status);
+    }
+    if (status != JNI_OK) {
+        return false;
+    }
+    return true;
+}
+
+void releaseEnv() {
+    JNIEnv *env;
+    if (current_jvm->GetEnv((void **) &env, JNI_VERSION_1_6) == JNI_OK) {
+        env->DeleteGlobalRef(gl_command);
+        env->DeleteGlobalRef(gl_command_clazz);
+        env->DeleteGlobalRef(gl_resultListener_clazz);
+        env->DeleteGlobalRef(gl_result_listener);
+        current_jvm->DetachCurrentThread();
+    }
+}
+/**
+ * 在子线程中env->FindClass 总是返回null,
+ * 原因是 AttachCurrentThread 只会加载 java 系统中的class 不会加载自定义的class
+ * **/
+void run() {
+    JNIEnv *env = nullptr;
+    if (getEnv(&env)) {
+
+        jobject paths = env->GetObjectField(gl_command,
+                                            env->GetFieldID(gl_command_clazz, "paths",
+                                                            "Ljava/util/Map;"));
+        jint type = env->GetIntField(gl_command,
+                                     env->GetFieldID(gl_command_clazz, "type", "I"));
+
+        auto *pHandler = new VideoHandler(env);
+        LOGD("videoCommandHandler", "command:%d", type);
+        int result_code = -1;
+        switch (type) {
+            case TYPE_SPLIT_SOUND:
+                result_code = pHandler->splitVideo(paths);
+                break;
+            case TYPE_VIDEO_JOINT:
+                result_code = pHandler->jointVideo(paths);
+                break;
+            case TYPE_ADD_INK:
+                result_code = pHandler->addInk(paths);
+                break;
+            case TYPE_DECODER:
+                result_code = pHandler->decode(paths);
+                break;
+            default:
+                break;
+
+        }
+
+        env->CallVoidMethod(gl_result_listener,
+                            env->GetMethodID(gl_resultListener_clazz, "onResult", "(I)V"),
+                            result_code);
+    }
+    releaseEnv();
+}
+
+JNIEXPORT void JNICALL videoCommandHandler(JNIEnv *env, jobject clazz, jobject command,
+                                           jobject result_listener) {
 
     jclass commandClazz = env->FindClass("com/ggg/handler/MediaHandler$VideoCommand");
-    jobject paths = env->GetObjectField(command,
-                                        env->GetFieldID(commandClazz, "paths", "Ljava/util/Map;"));
-    jint type = env->GetIntField(command,
-                                 env->GetFieldID(commandClazz, "type", "I"));
-    auto *pHandler = new VideoHandler(env);
-    LOGD("videoCommandHandler", "command:%d", type);
-    switch (type) {
-        case TYPE_SPLIT_SOUND:
-            return pHandler->splitVideo(paths);
+    jclass resultListenerClazz = env->FindClass(
+            "com/ggg/handler/MediaHandler$OnHandlerResultListener");
 
-        case TYPE_VIDEO_JOINT:
-
-            return pHandler->jointVideo(paths);
-
-        case TYPE_ADD_INK:
-            return pHandler->addInk(paths);
-
-        case TYPE_DECODER:
-            return pHandler->decode(paths);
-
-        default:
-            break;
-
-    }
-    return -1;
+    gl_command = env->NewGlobalRef(command);
+    gl_result_listener = env->NewGlobalRef(result_listener);
+    gl_command_clazz = static_cast<jclass>(env->NewGlobalRef(commandClazz));
+    gl_resultListener_clazz = static_cast<jclass>(env->NewGlobalRef(resultListenerClazz));
+    std::thread t(run);
+    t.join();
 }
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *jvm, void *reserved) {
     JNIEnv *env = nullptr;
+    current_jvm = jvm;
     if (jvm->GetEnv(reinterpret_cast<void **>(&env), JNI_VERSION_1_6) != JNI_OK || env == nullptr) {
         return -1;
     }
     jclass clazz = env->FindClass("com/ggg/handler/MediaHandler");
 
     JNINativeMethod methods[] = {{"sendVideoCommand",
-                                         "(Lcom/ggg/handler/MediaHandler$VideoCommand;)I",
+                                         "(Lcom/ggg/handler/MediaHandler$VideoCommand;Lcom/ggg/handler/MediaHandler$OnHandlerResultListener;)V",
                                          (void *) videoCommandHandler}};
 
     if (env->RegisterNatives(clazz, methods, sizeof(methods) / sizeof(methods[0]))) {
